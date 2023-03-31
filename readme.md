7e9c637494b8f7e737027bbb7234f4a0ffa23c2f
Kutbuddin khan <Kutbuddinkhan82@gmail.com>
2023-04-01 00:48:58 +0530

    First commit

---
+++ readme.md
@@ -0,0 +1,53 @@
+# Product Rating System
+#### Using Deep Learning
+<hr>
+
+## About
+__This Project generates rating of a product in an e-commerce website by analyzing the text based review provided by the users using deep learning.__ <br><br>
+<p align="center"> <img src="https://github.com/xitish/Product-Rating/blob/master/static/aa.gif"></p>
+
+The system uses sequence learning methodology in order to achieve desired functionality. The reviews provided by the users about a product on an e-commerce website is analyzed and rating of that product is generated based on the review. This system leverages the power of machine learning to completely eradicates the trouble of giving rating as well as writing review and helps to predict accurate rating based on user reviews. <br><br>
+<a href="https://drive.google.com/file/d/15NL7WCjxYIbqRguM62V44d2FwRiwrv2X/view?usp=sharing" > Learn More </a>
+
+## Installation
+The application runs on Python's __Flask__ micro  web framework. The following packages are required to be installed in order to run this application.
+> __Packages for Sentiment Analysis__
+  - numpy
+  - pandas
+  - tqdm
+  - tensorflow
+  - sklearn
+  - matplotlib
+  - keras
+  - nltk
+  
+  > __Packages for Web Application__
+  - flask
+  - SQLAlchemy
+  - Flask-MySQL
+  - mysqlclient
+  
+Install these packages using <code>pip</code> (i.e. <code> pip install numpy</code>)
+
+If problems arises during installing <code>mysqlclient</code> (eg: Microsoft Visual C++ 14.0 is required), visit <a href="https://www.lfd.uci.edu/~gohlke/pythonlibs/#mysqlclient">this </a> link and download .whl file for your system and python version and run <br>
+<code>pip install c:\location_to_whl_file\mysqlclient‑1.3.13‑cp36‑cp36m‑win_amd64.whl</code>
+
+Download __LSTM Model__ and __Text Data__ frome <a href="https://drive.google.com/drive/folders/1M5-5ITbUmj2BA8fUfMDkBG7OiSJLTMKq?usp=sharing"> here </a>and place them inside the folder containing above files.
+
+The folder should look as follows:
+<p align="center"> <img src="https://github.com/xitish/Product-Rating/blob/master/static/a.jpg"></p>
+
+> __Database__
+  - Create a database named `rating` in your SQL Server (database and table names can be changed inside `rating.sql` file). 
+  - Import the `rating.sql` into your SQL Database.
+
+## Usage
+Open __Command Prompt (CMD)__ and navigate to the folder containing the files. Then run
+<code>flask run</code>
+OR
+<code>python app.py</code>
+
+> Visit http://127.0.0.1:5000 in your browser to use the application.
+
+
+
---
+++ static/js/main.js
@@ -0,0 +1,135 @@
+/*
+	Snapshot by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+(function($) {
+
+	skel.breakpoints({
+		xlarge: '(max-width: 1680px)',
+		large: '(max-width: 1280px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Scrolly.
+			$('.scrolly').scrolly();
+
+	});
+
+})(jQuery);
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Text", "Based", "Sentiment", "Analysis"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings:  ["Text", "Based", "Sentiment", "Analysis"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+
+});
+window.oncontextmenu = function () {
+    return false;
+}
+$(document).keydown(function (event) {
+    if (event.keyCode == 123) {
+        alert("Hello! I am an alert box!!");
+        return false;
+    }
+    else if ((event.ctrlKey && event.shiftKey && event.keyCode == 73) || (event.ctrlKey && event.shiftKey && event.keyCode == 74)) {
+        alert("Hello! I am an alert box!!");
+        return false;
+    }
+});
